d344f14f3a11715c6fa605b683566281f710eae6
Dharani <dubbadharani@gmail.com>
2025-01-08 14:33:33 +0530

    Identation

--- product-service/src/main/java/com/programming/productservice/service/ProductService.java
+++ product-service/src/main/java/com/programming/productservice/service/ProductService.java
@@ -7,6 +7,7 @@ import com.programming.productservice.model.Product;
 import com.programming.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.internal.Logger;
